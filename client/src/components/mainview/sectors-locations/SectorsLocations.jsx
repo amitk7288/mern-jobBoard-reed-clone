3e39523320bedafc7b83bdb8be1ce09fe3e4a780
amitk7288 <amitkadara@gmail.com>
2024-10-24 11:31:46 +0100

    6-homepage-sector-location

---
+++ client/src/components/mainview/sectors-locations/SectorsLocations.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import SecLocGridItem from "./SecLocGridItem";
+import secLoc from "../../../data/SecLoc";
+
+export default function SectorsLocations() {
+  const [isSector, setIsSector] = useState(true)
+  const [isLocation, setIsLocation] = useState(false)
+
+  function handleClickSector(e) {
+    e.preventDefault();
+    setIsSector(true);
+    setIsLocation(false);
+  }
+  function handleClickLocation(e) {
+    e.preventDefault();
+    setIsLocation(true);
+    setIsSector(false);
+  }
+
+  return (
+    <div className="px-[23px] md:mx-[15px] md:px-[15px] lg:grid lg:justify-center flex flex-col gap-4">
+      <div className="flex justify-center gap-[30px] text-lg">
+        <button
+          onClick={handleClickSector}
+          href="#"
+          className={`flex-col justify-end gap-2 p-3 font-medium text-rdlightBlue ${isSector && `text-rdpink after:block after:w-full after:border-b-[5px] after:border-rdpink after:content-['']`} `}
+        >
+          Sectors
+        </button>
+        <button
+          onClick={handleClickLocation}
+          href="#"
+          className={`flex-col justify-end gap-2 p-3 font-medium text-rdlightBlue ${isLocation && `text-rdpink after:block after:w-full after:border-b-[5px] after:border-rdpink after:content-['']`} `}
+        >
+          Locations
+        </button>
+      </div>
+      <div className="lg:grid-rows-0 grid lg:max-w-[1280px] grid-cols-2 grid-rows-auto gap-5 md:gap-10 lg:grid-cols-4">
+        {/* Sectors */}
+        {isSector && secLoc[0].map((i) => (
+          <SecLocGridItem key={i.id} title={i.title} img={i.img} />
+        ))}
+        {/* Locations */}
+        {isLocation && secLoc[1].map((i) => (
+          <SecLocGridItem
+            key={i.id}
+            title={i.title}
+            img={i.img}
+            link={i.link}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ client/src/data/SecLoc.js
@@ -0,0 +1,65 @@
+import tech from "../assets/technology.jpg";
+import grad from "../assets/graduates.jpg";
+import eng from "../assets/engineering.jpg";
+import health from "../assets/health.jpg";
+import lon from "../assets/london.jpg";
+import man from "../assets/manchester.jpg";
+import newc from "../assets/newcastle.jpg";
+import liv from "../assets/liverpool.jpg";
+
+const secLoc = [
+  [
+    {
+      id: 1,
+      title: "Technology",
+      img: tech,
+      link: "",
+    },
+    {
+      id: 2,
+      title: "Graduates",
+      img: grad,
+      link: "",
+    },
+    {
+      id: 3,
+      title: "Engineering",
+      img: eng,
+      link: "",
+    },
+    {
+      id: 4,
+      title: "Health",
+      img: health,
+      link: "",
+    }
+  ],
+  [
+    {
+      id: 1,
+      title: "London",
+      img: lon,
+      link: "",
+    },
+    {
+      id: 2,
+      title: "Manchester",
+      img: man,
+      link: "",
+    },
+    {
+      id: 3,
+      title: "Newcastle",
+      img: newc,
+      link: "",
+    },
+    {
+      id: 4,
+      title: "Liverpool",
+      img: liv,
+      link: "",
+    }
+  ],
+]
+
+export default secLoc;
