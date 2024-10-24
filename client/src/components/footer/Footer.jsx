9faa030e85f0aeaca2c0caa8c24b18217df41da8
amitk7288 <amitkadara@gmail.com>
2024-10-24 13:25:05 +0100

    some updates to footer

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import lovemondaysImg from "../../assets/love-mondays.png";
 
 export default function Footer() {
   const [openSection, setOpenSection] = useState(null);
@@ -135,43 +136,59 @@ export default function Footer() {
           </nav>
         </li>
       </ul>
-      <ul className="hidden bg-rdblue lg:grid lg:min-w-[1280px] lg:grid-cols-5">
-        <li>
-          <h3 className="mb-5 font-medium text-white">JOBS</h3>
-          <nav className="flex flex-col gap-2 text-sm font-medium text-white">
-            <a href="#">Link</a>
-            <a href="#">Link</a>
-          </nav>
-        </li>
-        <li>
-          <h3 className="mb-5 font-medium text-white">RECRUITER</h3>
-          <nav className="flex flex-col gap-2 text-sm font-medium text-white">
-            <a href="#">Link</a>
-            <a href="#">Link</a>
-          </nav>
-        </li>
-        <li>
-          <h3 className="mb-5 font-medium text-white">COURSES</h3>
-          <nav className="flex flex-col gap-2 text-sm font-medium text-white">
-            <a href="#">Link</a>
-            <a href="#">Link</a>
-          </nav>
-        </li>
-        <li>
-          <h3 className="mb-5 font-medium text-white">MORE FROM Reed.co.uk</h3>
-          <nav className="flex flex-col gap-2 text-sm font-medium text-white">
-            <a href="#">Link</a>
-            <a href="#">Link</a>
-          </nav>
-        </li>
-        <li>
-          <h3 className="mb-5 font-medium text-white">REED</h3>
-          <nav className="flex flex-col gap-2 text-sm font-medium text-white">
-            <a href="#">Link</a>
-            <a href="#">Link</a>
-          </nav>
-        </li>
-      </ul>
+      <div className="flex flex-col gap-5">
+        <ul className="hidden bg-rdblue lg:grid lg:min-w-[1024px] lg:grid-cols-5 lg:gap-5">
+          <li>
+            <h3 className="mb-5 font-medium text-white">JOBS</h3>
+            <nav className="flex flex-col gap-2 text-sm font-medium text-white">
+              <a href="#">Link</a>
+              <a href="#">Link</a>
+            </nav>
+          </li>
+          <li>
+            <h3 className="mb-5 font-medium text-white">RECRUITER</h3>
+            <nav className="flex flex-col gap-2 text-sm font-medium text-white">
+              <a href="#">Link</a>
+              <a href="#">Link</a>
+            </nav>
+          </li>
+          <li>
+            <h3 className="mb-5 font-medium text-white">COURSES</h3>
+            <nav className="flex flex-col gap-2 text-sm font-medium text-white">
+              <a href="#">Link</a>
+              <a href="#">Link</a>
+            </nav>
+          </li>
+          <li>
+            <h3 className="mb-5 font-medium text-white">MORE</h3>
+            <nav className="flex flex-col gap-2 text-sm font-medium text-white">
+              <a href="#">Link</a>
+              <a href="#">Link</a>
+            </nav>
+          </li>
+          <li>
+            <h3 className="mb-5 font-medium text-white">REED</h3>
+            <nav className="flex flex-col gap-2 text-sm font-medium text-white">
+              <a href="#">Link</a>
+              <a href="#">Link</a>
+            </nav>
+          </li>
+        </ul>
+        <div className="flex flex-col items-center justify-center py-[20px] lg:items-start">
+          <div>
+            <img
+              src={lovemondaysImg}
+              alt="love mondays"
+              className="w-[180px] lg:relative lg:right-[10px]"
+            />
+          </div>
+          <div>
+            <p className="text-center text-xs text-white">
+              COPYRIGHT Reed.co.uk
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
