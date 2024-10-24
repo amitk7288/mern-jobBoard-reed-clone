0083ca96111f2a6f31ee6fed128cffd1745b40be
amitk7288 <amitkadara@gmail.com>
2024-10-24 10:24:30 +0100

    completed the ui for homepage trending

--- client/src/components/mainview/MainView.jsx
+++ client/src/components/mainview/MainView.jsx
@@ -1,10 +1,12 @@
 import Search from "./search/Search";
 import JobCard from "../JobCard";
+import Trending from "./trending/Trending";
 
 export default function MainView() {
   return (
     <>
       <Search />
+      <Trending />
       <JobCard />
     </>
   );
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -19,6 +19,7 @@ export default {
         rdblack: "#0f151a",
         rdpink: "#ff00cd",
         rdblue: "#081351",
+        rdlightBlue : "#1e1ef0",
       },
       screens: {
         s: "575px",
